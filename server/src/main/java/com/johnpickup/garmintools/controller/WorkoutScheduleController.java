4a2d91ceb06f0e679e6b8ba46e7ea4d6410a10c9
John Pickup <home@johnpickup.com>
2025-10-22 19:40:35 +0100

    sort out headers

--- server/src/main/java/com/johnpickup/garmintools/controller/WorkoutScheduleController.java
+++ server/src/main/java/com/johnpickup/garmintools/controller/WorkoutScheduleController.java
@@ -23,31 +23,35 @@ public class WorkoutScheduleController {
     ExcelToFitZip excelToFitZip;
 
     @PostMapping
-    public ResponseEntity<byte[]> convertExcelToFitZip(@RequestPart MultipartFile excelFile) {
-        log.info("Convert Excel file: {} ({})", excelFile.getOriginalFilename(), excelFile.getSize());
+    public ResponseEntity<byte[]> convertExcelToFitZip(@RequestPart MultipartFile file) {
+        log.info("Convert Excel file: {} ({})", file.getOriginalFilename(), file.getSize());
 
-        String extension = FilenameUtils.getExtension(excelFile.getOriginalFilename());
+        String extension = FilenameUtils.getExtension(file.getOriginalFilename());
         if (extension == null) {
             throw new RuntimeException("File has no filename");
         }
 
         try {
-            log.info("Reading {}", excelFile.getOriginalFilename());
-            String outputFilename = excelFile.getOriginalFilename().replace(".xlsx", ".zip").replace(".xls", ".zip");
+            log.info("Reading {}", file.getOriginalFilename());
+            String outputFilename = file.getOriginalFilename().replace(".xlsx", ".zip").replace(".xls", ".zip");
             byte[] bytes = switch (extension.toLowerCase()) {
-                case "xls" -> excelToFitZip.convertXls(excelFile.getInputStream());
-                case "xlsx" -> excelToFitZip.convertXlsx(excelFile.getInputStream());
+                case "xls" -> excelToFitZip.convertXls(file.getInputStream());
+                case "xlsx" -> excelToFitZip.convertXlsx(file.getInputStream());
                 default -> throw new RuntimeException("Unknown filetype " + extension);
             };
+            log.info("Response file: {} ({} bytes)", outputFilename, bytes.length);
 
             return ResponseEntity.ok()
                     .header("Content-Disposition", "attachment; filename=" + outputFilename)
+                    .header("Access-Control-Allow-Origin", "*")
+                    .header("Access-Control-Expose-Headers", "*")
+                    .header("Access-Control-Allow-Headers", "*")
                     .contentLength(bytes.length)
                     .contentType(MediaType.APPLICATION_OCTET_STREAM)
                     .body(bytes);
 
         } catch (IOException e) {
-            log.error("Error processing Excel file upload of {}", excelFile.getOriginalFilename(), e);
+            log.error("Error processing Excel file upload of {}", file.getOriginalFilename(), e);
             throw new RuntimeException("File is not a valid training schedule");
         }
     }
